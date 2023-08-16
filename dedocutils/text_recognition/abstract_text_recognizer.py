2e55ec2a72a9dd91b6e8e2af906e8012918ad34b
Nikita Shevtsov <shevtsov@ispras.ru>
2023-08-16 16:46:34 +0300

    upgrade AbstractTextRecognizer

--- dedocutils/text_recognition/abstract_text_recognizer.py
+++ dedocutils/text_recognition/abstract_text_recognizer.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import List, Optional
 
 import numpy as np
+
+from dedocutils.data_structures import BBox
 
 
 class AbstractTextRecognizer(ABC):
@@ -18,3 +20,18 @@ class AbstractTextRecognizer(ABC):
         :return: recognized text
         """
         pass
+
+    def recognize_bbox(self, image: np.ndarray,
+                       box: BBox,
+                       need_rotate: bool = False) -> str:
+        line_img = image[box.y_top_left:box.y_bottom_right, box.x_top_left:box.x_bottom_right]
+
+        if need_rotate:
+            line_img = np.rot90(line_img)
+
+        line_img = np.pad(line_img, [(15, 15), (15, 15), (0, 0)], constant_values=255)
+        text = self.recognize(line_img)
+        return text
+
+    def recognize_bboxes(self, image: np.ndarray, bboxes: List[BBox]) -> List[str]:
+        return [self.recognize_bbox(image, bbox) for bbox in bboxes]
